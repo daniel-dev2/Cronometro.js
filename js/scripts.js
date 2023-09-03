697133f73deb96d552777742d18b1f5cfc5020a0
Daniel <danielrodrigo098@gmail.com>
2023-09-02 21:51:58 -0300

    Adicionada nova função de marcar tempo e agora o cronometro também mede horas

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+const hoursEL = document.querySelector("#hours")
 const minutesEL = document.querySelector("#minutes");
 const secondsEL = document.querySelector("#seconds");
 const millisecondsEL = document.querySelector("#milliseconds");
@@ -6,8 +6,11 @@ const startBtn = document.querySelector("#startBtn");
 const pauseBtn = document.querySelector("#pauseBtn");
 const resumeBtn = document.querySelector("#resumeBtn");
 const resetBtn = document.querySelector("#resetBtn");
+const markBtn = document.querySelector("#markBtn");
 
 let interval;
+let presses = 0;
+let hours = 0
 let minutes = 0
 let seconds = 0
 let milliseconds = 0
@@ -17,7 +20,7 @@ startBtn.addEventListener("click", startTimer)
 pauseBtn.addEventListener("click", pauseTimer)
 resumeBtn.addEventListener("click", resumeTimer)
 resetBtn.addEventListener("click", resetTimer)
-
+markBtn.addEventListener("click", markTime)
 
 function startTimer() {
 
@@ -38,6 +41,13 @@ function startTimer() {
                 seconds = 0
             }
 
+            if (minutes === 60) {
+                hours++
+                minutes = 0
+                
+            }
+
+            hoursEL.textContent = formatTime(hours)
             minutesEL.textContent = formatTime(minutes)
             secondsEL.textContent = formatTime(seconds)
             millisecondsEL.textContent = formatMilliseconds(milliseconds)
@@ -53,6 +63,7 @@ function startTimer() {
 function pauseTimer() {
     isPaused = true
     pauseBtn.style.display = "none"
+    markBtn.style.display = "none"
     resumeBtn.style.display = "block"
 }
 
@@ -61,22 +72,42 @@ function resumeTimer() {
     isPaused = false
     resumeBtn.style.display = "none"
     pauseBtn.style.display = "block"
+    markBtn.style.display = "block"
 }
 
 
 function resetTimer() {
     clearInterval(interval)
+    hours = 0
     minutes = 0
     seconds = 0
     milliseconds = 0
+    presses = 0
 
+    hoursEL.textContent = "00"
     minutesEL.textContent = "00"
     secondsEL.textContent = "00"
     millisecondsEL.textContent = "000"
 
     startBtn.style.display = "block"
+    markBtn.style.display = "block"
     resumeBtn.style.display = "none"
     pauseBtn.style.display = "none"
+    document.getElementById("lap1").innerHTML = ""
+    document.getElementById("lap2").innerHTML = ""
+    document.getElementById("lap3").innerHTML = ""
+}
+
+function markTime() {
+    presses++ // user pressed the button
+    if (presses === 1) {
+        document.getElementById("lap1").innerHTML = `1º ${hoursEL.textContent} : ${minutesEL.textContent} : ${secondsEL.textContent} : ${millisecondsEL.textContent}`
+    } else if (presses === 2) {
+        document.getElementById("lap2").innerHTML = `2º ${hoursEL.textContent} : ${minutesEL.textContent} : ${secondsEL.textContent} : ${millisecondsEL.textContent}`
+    } else if (presses === 3) {
+        document.getElementById("lap3").innerHTML = `3º ${hoursEL.textContent} : ${minutesEL.textContent} : ${secondsEL.textContent} : ${millisecondsEL.textContent}`
+    }   
+    
 }
 
 function formatTime(time) {
